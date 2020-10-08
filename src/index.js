5b51cda80e45c2af8dbee9a3aa6985ffc735c2cc
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-08 18:29:21 -0500

    use react with webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 //import random number
 import { randomNumber } from './random.js';
+//import react
+import React from 'react';
+//import react dom
+import ReactDOM from 'react-dom';
 
-console.log(randomNumber());
+// console.log(randomNumber());
 
 document.getElementById('app').textContent = randomNumber();
+
+ReactDOM.render('hey there', document.getElementById('app'));
