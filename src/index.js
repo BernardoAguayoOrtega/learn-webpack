e103ec54251bca669537b09c1ca2e828cb91e3e9
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-08 19:08:41 -0500

    try to use styled components

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import { randomNumber } from './random.js';
 import React from 'react';
 //import react dom
 import ReactDOM from 'react-dom';
+//import styles
+import 'index.css';
 
 // console.log(randomNumber());
 
 document.getElementById('app').textContent = randomNumber();
 
-ReactDOM.render('hey there', document.getElementById('app'));
+ReactDOM.render(<h1>Hey there</h1>, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,15 @@ module.exports = {
 		rules: [
 			{
 				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-react'],
+					},
+				},
 			},
 		],
 	},
+	plugins: ['babel-plugin-styled-components'],
 };
